f4304492f92ef48a1e1a1cdf386b11b3e27d48fc
D4niel-dv <daniel.p.araujo@iesb.edu.br>
2024-10-17 20:59:11 -0300

    atividade 8

---
+++ 07-crud/src/app/page.js
@@ -0,0 +1,12 @@
+import Pagina from "@/components/Pagina"; // Assuming you have a path alias for the src directory
+
+
+
+export default function HomePage() {
+  return (
+    <Pagina titulo="Página Inicial">
+      
+
+    </Pagina>
+  );
+}
